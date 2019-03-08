cd82df9ce56e4ef9ad9a05f4d29de6da729e4d2e
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-03-08 13:33:49 +0100

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gato</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div>
+    </div>
+</body>
+</html>
