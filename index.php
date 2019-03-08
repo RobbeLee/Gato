973f431070de1121bd1afe7d62011c7d8c4e999b
MikeS25933 <25933@ma-web.nl>
2019-03-08 15:02:55 +0100

    Made navigation bar
    Includes:
    - logo
    - home & about links
    - search
    - new post & notification buttons
    - pfp

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <?php require 'include/meta.php'; ?>
     <title>Gato</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
 </head>
 <body>
+    <?php require 'include/nav.php'; ?>
     <div>
     </div>
 </body>
