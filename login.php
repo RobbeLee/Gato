93a72a194c7760b7ed6652d2534f00325ec76fa5
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-03-13 16:44:59 +0100

    i made a login.php
    i also made a small "don't have an account yet" button that re-directs to a localhost version of the signup.php

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
-<!-- "Matthijs" ik heb de signup.php verbeterd, nu moet het alleen nog aan een database gekoppeld worden. -->
-<!-- Mike - 13/03/19 - Heb php_header toe gevoegt en heb de titel veranderd -->
+<!-- 'Matthijs' 13-03-2019, ik heb de login pagina gemaakt en de "dont have an account yet" gekoppeld aan de local host van signup-->
 
 <?php require 'include/php_header.php'; ?>
 <!DOCTYPE html>
@@ -13,17 +12,19 @@
     <div id="container">
             <img src="assets/images/logo/hop_github_logo.svg" alt="logo" id="body__container__logo">
         <div id="div__container__tekst">
-            <h1>Registration</h1>
+            <h1>Login</h1>
         </div>
         <form method="POST" action="api/account/signup.php">
             <input type="text" name="email" placeholder="Email" class="div__container__info">
-            <input type="text" name="name" placeholder="Full Name" class="div__container__info">
-            <input type="text" name="username" placeholder="Username" class="div__container__info">
             <input type="text" name="password" placeholder="Password" class="div__container__info">
         </form>
         <!--<input type="button" class="button" value="Register"> -->
         <form action="http://localhost:8888/GitHub/Gato/">
-        <button class="button" type="submit" name="submit" >Register</button>
+        <button class="button" type="submit" name="submit" >Login</button>
+         <div id="div__container__registration">
+            <h4>Don't have an account yet?</h4>
+            <h5>Click <a href="http://localhost:8888/GitHub/gato/signup.php">here</a></h5> 
+        </div>
   </form> 
    </div>
 </body>
