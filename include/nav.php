40c2124068906fd607f6d82a09f7311237db4907
MikeS25933 <25933@ma-web.nl>
2019-03-15 14:50:33 +0100

    Added user nav & border radius change on search field

--- include/nav.php
+++ include/nav.php
@@ -19,8 +19,22 @@
         </form>
         <button class="nav__btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM17 11h-4v4h-2v-4H7V9h4V5h2v4h4v2z"/></svg></button>
         <button class="nav__btn"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/></svg></button>
-        <button class="nav__pfp-container">
+        <button class="nav__pfp-container" id="pfp-toggle">
             <img src="./assets/userfiles/pfp/<?php // Need unique ID here ?>default.webp" alt="error" class="nav__pfp">
         </button>
     </div>
 </nav>
+
+<div class="user-nav">
+    <div class="user-nav__info">
+        <h2 class="user-nav__username">@<?=$_SESSION['username']; ?></h2>
+        <span class="user-nav__email"><?=$_SESSION['email']; ?></span>
+    </div>
+    <div class="user-nav__body">
+        <a href="./u/<? // unique id here lol ?>" class="user-nav__link">my profile</a>
+        <a href="./u/<? // unique id here lol ?>" class="user-nav__link">my posts</a>
+        <a href="./u/<? // unique id here lol ?>/liked" class="user-nav__link">liked posts</a>
+        <a href="./account/settings" class="user-nav__link">settings</a>
+        <a href="./logout" class="user-nav__link">sign out</a>
+    </div>
+</div>
