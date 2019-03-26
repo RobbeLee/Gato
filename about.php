5f270e883bd85495078f44203824514ab775fa7e
Robbe <26785@ma-web.nl>
2019-03-26 14:08:32 +0100

    Begin About page.
    The beginnings of our about page.

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+    require 'include/php_header.php';
+    $subFolder = false;
+    $_TITLE = "About | ".$_BRAND;
+    $_PAGE = "about";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require 'include/meta.php'; ?>
+</head>
+<body>
+    <h1> About GATO </h1>
+    <p> With this page we hope to explain to you who we are, what we do, what this site is and what we hope to achieve. </p>
+    
+</body>
+</html>
