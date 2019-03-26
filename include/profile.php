457e352724cd8a9d9a2a8192ef01b9a94fcec922
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-03-26 13:59:52 +0100

    profile php

--- include/profile.php
+++ include/profile.php
@@ -5,16 +5,33 @@ $subFolder = true;
 $user = basename($_URL);
 $_TITLE = ucwords($user)." | ".$_BRAND;
 $_PAGE = "profile";
+
+// SELECT * FROM users WHERE username=?;
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <?php require '../include/meta.php'; ?>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/profile.css">
 </head>
 <body>
     <?php require '../include/nav.php'; ?>
+    <div id="container">
+    <div id="container__pfp">
+    <img src="./assets/userfiles/pfp/default.webp" alt="error" class="container__pfp">
+    <div id="container__info">
+        <h2 class="user-nav__username">@<?=$_SESSION['username']; ?></h2>
+        <span class="user-nav__email"><?=$_SESSION['email']; ?></span>
+    </div>
+</div>
     <div>
     </div>
+    <div id="container__href">
+    <a href="./" target="_blank" class="href">POST</a>
+    <a href="./" target="_blank" id="ahref" class="href">FAVORITES</a>
+    <a href="./" target="_blank" class="href">FRIENDS</a>
+</div>
 </body>
 </html>
