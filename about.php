328c5a4ef31317fe7b3be57778274907aa7011f5
Robbe <26785@ma-web.nl>
2019-03-27 15:00:31 +0100

    Updated about.php
    Last update as we have finalized the text. Css coming soon.

--- about.php
+++ about.php
@@ -13,20 +13,46 @@
     <?php require 'include/nav.php'; ?>
     <div class="wrapper">
         <h1> About GATO </h1>
-        <p> With this page we hope to explain to you who we are, what we do, what this site is and what we hope to achieve. </p>
-        <h2> Who are we </h2>
-        <p>Our names are Matthijs, Mike and Robbe. 
-            We are three Dutch students, who do a mediadevelopment course. <br>
-            During this course we get several assigments and this is one of them.</p> <br> <br>
-        <div class="namen">
-            <h3>Matthijs</h3>
-            <p><i>stukje over mij</i></p> <br>
-            <h3>Mike</h3>
-            <p><b><i>nee</i></b></p> <br>
-            <h3>Robbe</h3>
-            <p>Hi my name is Robbe and I am a 16 year student from the Netherlands.
-                I did all of the documentation and this about page for our project.</p>
-        </div>    
+        <p> With this page we hope to explain to you who we are, what we do, what this site is and what we hope to achieve. </p><br>
+        <div class="whatIsThisSite">
+            <h2>What is this site?</h2>
+            <p>With Gato we hope to build a site that's easy to navigate and use. We see many social media platforms
+                That are just impossible to use. <br>They are busy and often have annoying popups. Here at Gato we see this
+                and get a little frustrated about it, we will offer you a place where you just come to share a photo and
+                look at the photo's of your friends.<br> Nothing more, nothing less. This way we can offer you minimalistic design
+                that is easy to use and navigate.</p>
+        </div>
+        <div class="whoWeAre">
+            <h2> Who are we </h2>
+            <p>Our names are Matthijs, Mike and Robbe. 
+                We are three Dutch students, who do a mediadevelopment course. <br>
+                During this course we get several assigments and this is one of them.</p> <br> <br>
+            <div class="names">
+                <h3>Matthijs</h3>
+                <p><i>stukje over mij</i></p> <br>
+                <h3>Mike</h3>
+                <p><b><i>nee</i></b></p> <br>
+                <h3>Robbe</h3>
+                <p>Hi my name is Robbe and I am a 16 year student from the Netherlands.
+                    I did all of the documentation and this about page for our project.</p>
+            </div>
+        </div>
+        <div class="whatWeDo">
+            <h2> What we do</h2>
+            <p>As said earlier, we are still students. We currently study mediadevelopment at MediaCollege Amsterdam.<br>
+                This is a three year course in which we will have to do many projects, this so happens to be one of them.<br>
+                Over the three years we will make a ton of project so if you want to see the rest, take a look at 
+                our GitHub links.<br></p>
+                <a href="https://github.com/263782" target="_blank">Matthijs</a><br>
+                <a href="https://github.com/MikeS25933" target="_blank">Mike</a><br>
+                <a href="https://github.com/RobbeLee" target="_blank">Robbe</a>
+        </div>
+        <div class="whatWeAchive">
+            <h2>What we hope to achieve</h2>
+            <p>We hope that we can offer you a platform where you can share your photo's easy and fast.<br>
+                We can only do this with your feedback tho so if you have any issues or remarks please contact us.
+                </p>
+        </div>
     </div>
 </body>
 </html>
