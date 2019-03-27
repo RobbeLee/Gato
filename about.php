26281adfad637c0fc4c607244714dd1b0dd6a47a
Robbe <26785@ma-web.nl>
2019-03-27 10:26:06 +0100

    Updated about css and php.
    Added padding and text.

--- about.php
+++ about.php
@@ -17,12 +17,12 @@
         <h2> Who are we </h2>
         <p>Our names are Matthijs, Mike and Robbe. 
             We are three Dutch students, who do a mediadevelopment course. <br>
-            During this course we get several assigments and this is one of them.</p>
+            During this course we get several assigments and this is one of them.</p> <br> <br>
         <div class="namen">
             <h3>Matthijs</h3>
-            <p><i>stukje over mij</i></p>
+            <p><i>stukje over mij</i></p> <br>
             <h3>Mike</h3>
-            <p><b><i>nee</i></b></p>
+            <p><b><i>nee</i></b></p> <br>
             <h3>Robbe</h3>
             <p>Hi my name is Robbe and I am a 16 year student from the Netherlands.
                 I did all of the documentation and this about page for our project.</p>
