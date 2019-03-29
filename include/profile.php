0630c79150970c182054e22b679bac25f997e965
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-03-29 15:01:56 +0100

    other class names and id tags

--- include/profile.php
+++ include/profile.php
@@ -28,20 +28,21 @@ if (file_exists($path)) {
 </head>
 <body>
     <?php require '../include/nav.php'; ?>
-    <div id="container"> 
-    <div id="container__pfp">
-    <img src="<?=$userPfp?>" alt="error" class="container__pfp">
-    <div id="container__info">
-    <span class="user-nav__name"><?=$result['name']; ?></span>
-    <h2 class="user-nav__username">@<?=$result['username']; ?></h2>
+    <div class="header" id="container"> 
+        <div class="header__body">
+            <div class="header__pfp-container" id="container__pfp">
+                <img class="header__pfp" src="<?=$userPfp?>" alt="error" class="container__pfp">
+            </div>
+            <div id="header__info">
+                <h2 class="header__username">@<?=$result['username']; ?></h2>
+                <span class="header__name"><?=$result['email']; ?></span>
+            </div>
+        </div>
+        <div id="header__links-container">
+            <a href="./" target="_blank" class="header__link">POST</a>
+            <a href="./" target="_blank" id="ahref" class="header__link">FAVORITES</a>
+            <a href="./" target="_blank" class="header__link">FRIENDS</a>
+        </div>
     </div>
-</div>
-    <div>
-    </div>
-    <div id="container__href">
-    <a href="./" target="_blank" class="href">POST</a>
-    <a href="./" target="_blank" id="ahref" class="href">FAVORITES</a>
-    <a href="./" target="_blank" class="href">FRIENDS</a>
-</div>
 </body>
 </html>
