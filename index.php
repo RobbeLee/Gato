1a691ac1ce28ae28061fef3915ea41cb1e481412
Robbe <26785@ma-web.nl>
2019-04-09 14:09:33 +0200

    Uploaded makePost.php, postForm.php. Modified default.css, .gitgnore and index.php.
    Started the process for making posts.

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $_PAGE = "index";
     <?php require 'include/nav.php'; ?>
     <div>
     </div>
+    <?php require 'include/postForm.php' ?>
     <?php// require 'include/footer.php'; ?>
 </body>
 </html>
