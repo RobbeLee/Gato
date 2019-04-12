27eca7c61b127ebbf37aba48f527b9568a72a70e
MikeS25933 <25933@ma-web.nl>
2019-04-12 09:02:37 +0200

    Finished settings

--- api/settings/account.php
+++ api/settings/account.php
@@ -4,6 +4,7 @@ require '../../include/php_header.php';
 
 $errors = [];
 $updates = [];
+$values = [];
 
 if (!isset($_SESSION['id']) && !isset($_POST['submit'])) {
     header("Location: ../../?lenker");
@@ -17,6 +18,7 @@ if (!empty($_POST['name'])) {
         array_push($errors, 'Your name cannot be longer than 60 characters.');
     } else {
         $name = $_POST['name'];
+        array_push($values, $name);
         array_push($updates, 'name=?');
     }
 }
@@ -26,6 +28,7 @@ if (!empty($_POST['username'])) {
         array_push($errors, 'Your username cannot be longer than 60 characters.');
     } else {
         $username = $_POST['username'];
+        array_push($values, $username);
         array_push($updates, 'username=?');
     }
 }
@@ -35,11 +38,12 @@ if (!empty($_POST['email'])) {
         array_push($errors, 'Your email is invalid.');
     } else {
         $email = $_POST['email'];
+        array_push($values, $email);
         array_push($updates, 'email=?');
     }
 }
 
-if (!empty($_FILES)) {
+if ($_FILES['file']['size'] > 0) {
     if(!file_exists($_FILES['file']['tmp_name']) || !is_uploaded_file($_FILES['file']['tmp_name'])) {
         array_push($errors, 'File does not exist.');
     } else {
@@ -66,6 +70,33 @@ if (count($errors) > 0) {
     echo "<pre>";
     print_r($errors);
     exit;
+}
+
+if (count($updates) > 0) {
+    for ($i=0; $i < count($updates); $i++) {
+        if ($i !== count($updates) - 1) {
+            $sql = $sql.$updates[$i].", ";
+        } else {
+            $sql = $sql.$updates[$i];
+        }
+    }
+    $sql = $sql." WHERE id=?;";
+    array_push($values, $_SESSION['id']);
+
+    $stmt = $conn->prepare($sql);
+    if (!$stmt->execute($values)) {
+        throw new error("FATAL QUERY ERROR");
+        exit;
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE id=? LIMIT 1");
+    $stmt->execute([$_SESSION['id']]);
+    $result = $stmt->fetch();
+
+    $_SESSION['id'] = $result['id'];
+    $_SESSION['name'] = $result['name'];
+    $_SESSION['username'] = $result['username'];
+    $_SESSION['email'] = $result['email'];
 }
 
 header("Location: ../../account/settings");
