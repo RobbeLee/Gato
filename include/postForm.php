18c10b6f0ab8041dc33c0ece223a26b9882e94e5
Robbe <26785@ma-web.nl>
2019-04-12 13:28:13 +0200

    Updated postForm.php and default.css
    working on the styling of the modal.

--- include/postForm.php
+++ include/postForm.php
@@ -1,7 +1,7 @@
 <div id="myModal" style="display:none;">
     <div id="form-container">
         <div id="modal" class="form">
-            <span class="close">&times;</span>
+            <span class="form__close">&times;</span>
             <form enctype="multipart/form-data" method="GET" action="<?php if ($subFolder) echo "../"; ?>api/post/makePost.php">
                 <h1 class="form__title"> What do you want to post today? </h1>
                 <div class="form__content">
@@ -19,7 +19,7 @@
     //ik hoop dat iemand me uit mijn lijden verlost.
     let modal = document.querySelector('#myModal'),
         btn = document.querySelector("#new_post"),
-        span = document.querySelector(".close"),
+        span = document.querySelector(".form__close"),
         openModal = false;
 
     btn.addEventListener('click', () => {
@@ -27,6 +27,7 @@
         openModal ? openModal = false : openModal = true;
     });
     span.addEventListener('click', ()=>{
-        
+        openModal ? modal.style.display = "none" : modal.style.display = "flex";
+        openModal ? openModal = false : openModal = true;
     })
 </script> 
