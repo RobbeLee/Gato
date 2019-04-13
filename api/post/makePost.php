1e8ffddd27f6084ba9ebe91b175a113b8916ae2b
Robbe <26785@ma-web.nl>
2019-04-13 20:46:35 +0200

    Updated makePost.php
    Commented away some code and added errors.

--- api/post/makePost.php
+++ api/post/makePost.php
@@ -1,20 +1,4 @@
 <?php
-<<<<<<< HEAD
-    if(isset($_FILES['image'])){
-        $errors = array();
-        $file_name = $_FILES['image']['name'];
-        $file_size = $_FILES['image']['size'];
-        $file_tmp = $_FILES['image']['tmp_name'];
-        $file_type = $_FILES['image']['type'];
-    }
-    // de explode string-functie breekt een string in een array
-    // hierbij breek je de string na de . (punt) waardoor je de bestands type hebt
-        $filename_deel = explode('.',$_FILES['image']['name']);
-    // end laat de laatste waarde van de array zoen 
-        $bestandstype = end($filename_deel); 
-    // voor het geval er JPG ipv jpg is geschreven
-        $file_ext = strtolower($bestandstype);
-=======
     // if(isset($_FILES['image'])){
     //     $errors = array();
     //     $file_name = $_FILES['image']['name'];
@@ -29,7 +13,6 @@
     //     $bestandstype = end($filename_deel);
     // // voor het geval er JPG ipv jpg is geschreven
     //     $file_ext = strtolower($bestandstype);
->>>>>>> b98a79c3e4f84ce406563cb796da7a989e918f7a
     
     // $bestandstypen = ["jpeg","jpg","png"]; 
     // if(!in_array($file_ext,$bestandstypen)){
@@ -48,16 +31,16 @@
 
 if (isset($_FILES['image'])) {
     if(!file_exists($_FILES['file']['tmp_name']) || !is_uploaded_file($_FILES['file']['tmp_name'])) {
-        array_push($errors, 'File does not exist.');
+        echo "Sorry, you haven't chosen a file";
     } else {
         $allowed = ['png', 'jpg', 'jpeg', 'gif'];
         $fileExt = explode(".", $_FILES['file']['name']);
         $fileActExt = strtolower(end($fileExt));
         if (!in_array($fileActExt, $allowed)) {
-            array_push($errors, 'Unsupported file type.');
+            echo "sorry we don't support that kind of file.";
         } else {
             if ($_FILES['file']['size'] > 5097152) {
-                array_push($errors, 'File size too large.');
+                echo "Your file is too big, sorry.";
             } else {
                 $path = "../../assets/userfiles/imgs/".$_SESSION['id'].".webp";
                 move_uploaded_file($_FILES['file']['tmp_name'], $path);
