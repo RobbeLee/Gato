21f38fe05b4f7aa674b409015085625f7ca91c5d
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-04-17 14:10:13 +0200

    small changes from mike

--- include/nav.php
+++ include/nav.php
@@ -16,14 +16,23 @@ if (file_exists($path)) {
 }
 ?>
 <nav>
-    <div class="nav__left-container">
-        <h1 class="nav__title" title="<?=$_BRAND; ?>"><?=$_BRAND; ?></h1>
-        <a href="<?php if ($subFolder) echo "."; ?>./" title="Home" class="nav__link">home</a>
-        <a href="<?php if ($subFolder) echo "../"; ?>about" title="About" class="nav__link">about</a>
+
+
+
+
+
+<div class="nav__hamburger">
+
+        <div class="nav__left-container">
+                <h1 class="nav__title" title="<?=$_BRAND; ?>"><?=$_BRAND; ?></h1>
+                <a href="<?php if ($subFolder) echo "."; ?>./" title="Home" class="nav__link">home</a>
+                <a href="<?php if ($subFolder) echo "../"; ?>about" title="About" class="nav__link">about</a>
     </div>
+</div>
+
     <?php if (isset($_SESSION['id'])): ?>
     <div class="nav__right-container">
-        <form class="nav__form" action="<?php if ($subFolder) echo "../"; ?>search" method="get">
+        <form class="nav__form" action="<?php if ($subFolder) echo "."; ?>./search" method="get">
             <input class="nav__input" type="text" name="q" placeholder="Search" title="Search" autocomplete="off" autocorrect="off" spellcheck="false" autocapitalize="none"/>
             <button type="submit" title="Search" class="nav__search-btn">
                 <svg viewBox="0 0 18 18"><path d="m 9.3962688,9.5396955 3.1875032,3.1165125 4.685825,4.581467 Z M 10.126225,9.276927 C 10.957192,8.2289874 11.351645,6.8454225 11.198222,5.5168348 11.044799,4.188247 10.345391,2.930999 9.2974515,2.1000317 8.249512,1.2690645 6.8659471,0.87461216 5.5373593,1.0280348 4.2087716,1.1814574 2.9515235,1.8808656 2.1205563,2.9288052 1.2895891,3.9767448 0.89513671,5.3603097 1.0485594,6.6888974 1.201982,8.0174852 1.9013902,9.2747332 2.9493298,10.1057 c 1.0479395,0.830968 2.4315044,1.22542 3.7600922,1.071997 1.3285877,-0.153422 2.5858358,-0.85283 3.416803,-1.90077 z" /></svg>
