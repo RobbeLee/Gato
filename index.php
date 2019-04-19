b45326905b53a95061fafbd77094a06db9efa9d0
MikeS25933 <25933@ma-web.nl>
2019-04-19 23:43:13 +0200

    Redid posts styling on index

--- index.php
+++ index.php
@@ -33,19 +33,40 @@ $_PAGE = "index";
             
             $stmt->execute();
             $posts = $stmt->fetchAll();
-            foreach ($posts as $post): 
-        ?>
-        <div class="post">
-            <?php if (!is_null($post['uniqueid'])): ?>
-            <div class="post__img-container">
-                <img src="./assets/userfiles/imgs/<?=htmlspecialchars($post['uniqueid'])?>.webp" class="post__img">
-            </div>
-            <?php endif; ?>
-            <div class="post__content-container">
-                <p class="post__content"><?=htmlspecialchars($post['content'])?></p>
-            </div>
-        </div>
-        <?php endforeach; ?>
+            
+            foreach ($posts as $post): ?>
+                <?php 
+                    $stmt = $conn->prepare("SELECT * FROM users WHERE id=? LIMIT 1;");
+                    $stmt->execute([$post['uid']]);
+                    $user = $stmt->fetch();
+                    $pfp = "default";
+                    if (file_exists("assets/userfiles/pfp/".$user['id'].".webp")) {
+                        $pfp = $user['uniqueid'];
+                    }
+                ?>
+                <div class="post" <?php if (is_null($post['uniqueid'])) echo 'style="height:150px;"'; ?>>
+                        <?php if (!is_null($post['uniqueid'])): ?>
+                            <div class="post__img-container">
+                                <img src="assets/userfiles/imgs/<?=htmlspecialchars($post['uniqueid'])?>.webp" alt="error" class="post__img">
+                            </div>
+                        <?php endif; ?>
+                    <p class="post__content">
+                        <?=htmlspecialchars($post['content'])?>
+                    </p>
+                    <div class="post__body">
+                        <div class="post__pfp-container">
+                            <img class="post__pfp" src="assets/userfiles/pfp/<?=htmlspecialchars($pfp)?>.webp" alt="<?=htmlspecialchars($user['name'])?>" title="<?=htmlspecialchars($user['name'])?>">
+                        </div>
+                        <div class="post__user-container">
+                            <div style="display:flex;flex-direction:column;">
+                                <span class="post__name"><?=htmlspecialchars($user['name'])?></span>
+                                <span class="post__username">@<?=htmlspecialchars($user['username'])?></span>
+                            </div>
+                            <span class="post__date"><?php echo htmlspecialchars(date('F d, Y', strtotime($post['date']))); ?></span>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
     </div>
 </body>
 </html>
